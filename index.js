b3f0dd1e19692892517151a31e3407847e979c19
Anne <anneqxy2003@gmail.com>
2024-05-26 10:17:09 +0800

    testing

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express'); // import liabrary
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+app.use(
+   cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+   })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-/* get -> function; 
-   '/' -> trying to access '/', can be changed to '/grading';
-   (req, res) -> request and outgoing response;
-   res.send() -> immediately send some JSON to made their request
-*/
+
+require('./routes/authRoutes')(app);
+
+
+
+// app.get('/', (req, res) => {
+//     res.send({ bye: 'buddy' });
+// });
+
+// /* get -> function; 
+//    '/' -> trying to access '/', can be changed to '/grading';
+//    (req, res) -> request and outgoing response;
+//    res.send() -> immediately send some JSON to made their request
+// */
+
+
+// 
+
+
 
 const PORT = process.env.PORT || 5000; 
 /* for heroku to decide which prot used in last minute
