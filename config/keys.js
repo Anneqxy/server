b4c82157edaecfc339d4d183f70e998f49e502b8
Anne <anneqxy2003@gmail.com>
2024-05-27 15:17:04 +0800

    Fix MongoDB authentication issue

--- config/keys.js
+++ config/keys.js
@@ -1,19 +1,9 @@
-// if (process.env.NODE_ENV === 'production') {
-//     //we are in production -- return the prod set of keys
-//     module.exports = require('./prod');
-// } else {
-//     //we are in development -- return dev keys
-//     module.exports = require('./dev');
-// }
-
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {
-       googleClientID: process.env.GOOGLE_CLIENT_ID,
-       googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-       mongoURI: process.env.MONGO_URI,
-       cookieKey: process.env.COOKIE_KEY
-    };
- } else {
+    //we are in production -- return the prod set of keys
+    module.exports = require('./prod');
+} else {
+    //we are in development -- return dev keys
     module.exports = require('./dev');
- }
+}
+
  
