03687d8ae76e57c5d00eb6258c5cf691b48ad1d9
Anne <anneqxy2003@gmail.com>
2024-05-27 14:07:54 +0800

    finish auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-    googleClientID: '660657320029-sgo7q1o5g7ojvk5oklvc45a3h6b9jp96.apps.googleusercontent.com',
-    googleClientScrete: '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',
-    mongoURI: 'mongodb+srv://xque:iVn8iu2Ni9faNoPV@bankingweb.i3sdjup.mongodb.net/emialsend?retryWrites=true&w=majority&appName=BankingWeb',
-    cookieKey: 'qweasdzxcwerafawde',
-};
+if (process.env.NODE_ENV === 'production') {
+    //we are in production -- return the prod set of keys
+    module.exports = require('./prod');
+} else {
+    //we are in development -- return dev keys
+    module.exports = require('./dev');
+}
