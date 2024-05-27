768c842823c7028f620052481e7d3c071d18cafe
Anne <anneqxy2003@gmail.com>
2024-05-27 15:14:13 +0800

    Fix MongoDB authentication issue

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,19 @@
+// if (process.env.NODE_ENV === 'production') {
+//     //we are in production -- return the prod set of keys
+//     module.exports = require('./prod');
+// } else {
+//     //we are in development -- return dev keys
+//     module.exports = require('./dev');
+// }
+
 if (process.env.NODE_ENV === 'production') {
-    //we are in production -- return the prod set of keys
-    module.exports = require('./prod');
-} else {
-    //we are in development -- return dev keys
+    module.exports = {
+       googleClientID: process.env.GOOGLE_CLIENT_ID,
+       googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+       mongoURI: process.env.MONGO_URI,
+       cookieKey: process.env.COOKIE_KEY
+    };
+ } else {
     module.exports = require('./dev');
-}
+ }
+ 
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
+
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientScrete: process.env.GOOGLE_CLIENT_SCRETE,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
-};
+  };
