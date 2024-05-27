9b206e28e72bec654604a1352d01dfdcdee23644
Anne <anneqxy2003@gmail.com>
2024-05-27 10:12:31 +0800

    Fix module not found and syntax error

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '660657320029-sgo7q1o5g7ojvk5oklvc45a3h6b9jp96.apps.googleusercontent.com',
+    googleClientScrete: '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',
+    mongoURI: 'mongodb+srv://xque:iVn8iu2Ni9faNoPV@bankingweb.i3sdjup.mongodb.net/emialsend?retryWrites=true&w=majority&appName=BankingWeb',
+    cookieKey: 'qweasdzxcwerafawde',
+};
