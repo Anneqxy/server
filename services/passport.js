96d508ac26118f428eba7204c77d547f61a1bed9
Anne <anneqxy2003@gmail.com>
2024-05-31 11:23:22 +0800

    add new code

--- services/passport.js
+++ services/passport.js
@@ -1,44 +1,3 @@
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const mongoose = require('mongoose');
-// const keys = require('../config/keys');
-
-// const User = mongoose.model('users');
-
-// passport.serializeUser((user, done) => {
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//     User.findById(id).then(user => {
-//         done(null, user);
-//     });
-// });
-
-// passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: keys.googleClientID,
-//             clientSecret: keys.googleClientScrete,
-//             callbackURL: '/auth/google/callback'
-//         }, 
-//         (accessToken, refreshToken, profile, done) => {
-//             User.findOne({googleId: profile.id}).then((existingUser) =>{
-//                 if (existingUser) {
-//                     //We already have a record with the given profile ID
-//                     done(null, existingUser);
-//                 } else {
-//                     new User({googleId: profile.id})
-//                         .save()
-//                         .then(user => done(null, user));
-//                 }
-//             });
-//         }
-//     )
-// );
-
-
-
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
@@ -47,17 +6,12 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    console.log('Serialize User:', user);
     done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
-        console.log('Deserialize User:', user);
         done(null, user);
-    }).catch(err => {
-        console.error('Error deserializing user:', err);
-        done(err, null);
     });
 });
 
@@ -65,27 +19,19 @@ passport.use(
     new GoogleStrategy(
         {
             clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback',
-            proxy: true
-        },
+            clientSecret: keys.googleClientScrete,
+            callbackURL: '/auth/google/callback'
+        }, 
         async (accessToken, refreshToken, profile, done) => {
-            console.log('Access Token:', accessToken);
-            console.log('Profile:', profile);
-            try {
-                const existingUser = await User.findOne({ googleId: profile.id });
-                if (existingUser) {
-                    console.log('Existing user:', existingUser);
-                    return done(null, existingUser);
-                }
-
-                const user = await new User({ googleId: profile.id }).save();
-                console.log('New user:', user);
-                done(null, user);
-            } catch (error) {
-                console.error('Error in GoogleStrategy:', error);
-                done(error, null);
-            }
+            const existingUser = await User.findOne({googleId: profile.id});
+            if (existingUser) {
+                //We already have a record with the given profile ID
+                return done(null, existingUser);
+            } 
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
         }
     )
 );
+
+
