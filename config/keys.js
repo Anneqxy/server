08f11554460d833e579634ae919e0d91a84b94fb
Anne <anneqxy2003@gmail.com>
2024-05-31 18:38:07 +0800

    Remove hardcoded secrets and use environment variables

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     //we are in development -- return dev keys
-    module.exports = require('./dev');
+    module.exports = require('./.env');
 }
 
  
